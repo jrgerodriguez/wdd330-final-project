8a6f6c8461a77c98e1477eff6cf95efeec3d466c
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-10-16 08:05:32 -0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import { loadHeaderFooter } from "./js/utils.mjs";
-import ShowMenu from "./js/MainMenu.mjs";
-import { key, host } from "./js/config.js";
+import { loadHeaderFooterIndex } from "../js/utils.mjs";
+import ShowMenu from "../js/MainMenu.mjs";
+import { key, host } from "../js/config.js";
 
-loadHeaderFooter();
+loadHeaderFooterIndex();
 const showMenu = new ShowMenu(key,host, document.querySelector(".product-list"),);
     showMenu.init();
