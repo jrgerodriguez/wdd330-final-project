0567d5737fffff1557c421bbdd925156f528a24e
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-10-16 08:15:32 -0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import { loadHeaderFooterIndex } from "/js/utils.mjs";
-import ShowMenu from "/js/MainMenu.mjs";
-import { key, host } from "/js/config.js";
+import { loadHeaderFooterIndex } from "./js/utils.mjs";
+import ShowMenu from "./js/MainMenu.mjs";
+import { key, host } from "./js/config.js";
 
 loadHeaderFooterIndex();
 const showMenu = new ShowMenu(key,host, document.querySelector(".product-list"),);
