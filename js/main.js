99b5272f856e777b67d382e2a5824e17fa2ce121
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-10-16 10:10:50 -0600

    updated

--- js/main.js
+++ js/main.js
@@ -5,4 +5,3 @@ import { key, host } from "./config.js";
 loadHeaderFooter();
 const showMenu = new ShowMenu(key, host, document.querySelector(".product-list"),);
 showMenu.init();
-console.log("I am working")
